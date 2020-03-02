55ec200b452396f0fe88b858cc8999065d6ecc28
Robin Jungers <robin@jungers.fr>
2020-03-03 00:45:57 +0900

    Basic responsive

--- assets/videos/crop.sh
+++ assets/videos/crop.sh
@@ -1,6 +1,6 @@
 # Scale video (1)
-ffmpeg -i 1.original.mp4 -s 500x500 -c:a copy 1.mp4
+ffmpeg -i midnight.original.mp4 -s 500x500 -c:a copy midnight.mp4
 # Crop & scale horizontal video (2)
-ffmpeg -i 2.original.mp4 -filter:v crop=in_h:in_h:in_w/2-in_h/2:0 -s 500x500 -c:a copy 2.mp4
+ffmpeg -i baby.original.mp4 -filter:v crop=in_h:in_h:in_w/2-in_h/2:0 -s 500x500 -c:a copy baby.mp4
 # Crop & scale vertical video (3)
-ffmpeg -i 3.original.mp4 -filter:v crop=in_w:in_w:0:in_h/2-in_w/2 -s 500x500 -c:a copy 3.mp4
+ffmpeg -i asphyxiation.original.mp4 -filter:v crop=in_w:in_w:0:in_h/2-in_w/2 -s 500x500 -c:a copy asphyxiation.mp4
