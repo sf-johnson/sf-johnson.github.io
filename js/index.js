b9aca2176551d2c674ba5d9499756c903e7684fb
Robin Jungers <robin@jungers.fr>
2020-03-07 20:02:50 +0900

    Fixed video autoply on mobile, styling fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
-window.onload = function() {
+function fadeVolumeIn( audio, complete ) {
+	anime( {
+		targets : audio,
+		volume : [0.0, 1.0],
+		duration : 1000,
+		easing: 'linear',
+		complete : complete
+	} );
+}
+
+function fadeVolumeOut( audio, complete ) {
+	anime( {
+		targets : audio,
+		volume : 0.0,
+		duration : 1000,
+		easing : 'linear',
+		complete : complete
+	} );
+}
+
+window.addEventListener( 'DOMContentLoaded', function() {
 	var app = {
 		fades : document.querySelectorAll( '.fade' ),
 		videos : document.querySelectorAll( '.video' ),
@@ -6,14 +26,17 @@ window.onload = function() {
 		audios : document.querySelectorAll( '.audio' ),
 		audioActiveIndex : null,
 		tracks : document.querySelectorAll( '.track' ),
-		initEmail : function() {},
+		initEmail : function() {
+			var email = document.querySelector( '#email' );
+			var href = email.getAttribute( 'href' );
+
+			email.setAttribute( 'href', href.replace( '.at.', '@' ) );
+		},
 		initTracks : function() {
 			this.tracks.forEach( ( function( track, i ) {
-				if ( track.getAttribute( 'data-preview' ) === 'true' ) {
-					track.onclick = ( function() {
-						this.setActiveAudio( i );
-					} ).bind( this );
-				}
+				track.onclick = ( function() {
+					this.setActiveAudio( i );
+				} ).bind( this );
 			} ).bind( this ) );
 		},
 		setActiveVideo : function( i ) {
@@ -38,27 +61,31 @@ window.onload = function() {
 						audio.pause();
 						audio.currentTime = 0.0;
 					} ).bind( this );
+
+					fadeVolumeIn( audio );
 				} else {
-					audio.pause();
-					audio.currentTime = 0.0;
+					fadeVolumeOut( audio, function() {
+						audio.pause();
+						audio.currentTime = 0.0;
+					} );
 				}
 			} ).bind( this ) );
 		},
 		checkScrollVideos : function() {
-			var minDist = Infinity;
-			var minIndex = null;
+			var closestDist = Infinity;
+			var closestIndex = null;
 
 			this.tracks.forEach( function( track, i ) {
 				var scroll = window.pageYOffset + 0.5 * window.innerHeight;
 				var dist = Math.abs( scroll - track.offsetTop );
 
-				if ( dist < minDist ) {
-					minIndex = i;
-					minDist = dist;
+				if ( dist < closestDist ) {
+					closestDist = dist;
+					closestIndex = i;
 				}
 			} );
 
-			this.setActiveVideo( minIndex );
+			this.setActiveVideo( closestIndex );
 		},
 		checkScrollFades : function() {
 			this.fades.forEach( function( fade ) {
@@ -70,6 +97,11 @@ window.onload = function() {
 		}
 	};
 
+	console.assert(
+		( app.tracks.length === app.videos.length ) &&
+		( app.tracks.length === app.audios.length )
+	);
+
 	app.initEmail();
 	app.initTracks();
 
@@ -77,4 +109,4 @@ window.onload = function() {
 		app.checkScrollVideos();
 		app.checkScrollFades();
 	} )();
-}
+} );
